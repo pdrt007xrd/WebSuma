22df1790ef219fb9708fa25bd661fb9d4e65e2c8
P P <pedrito007xrd@gmail.com>
2022-06-18 20:35:53 -0400

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Calcular(Suma suma)
+    {
+        suma.Numero1 = 10;
+        suma.Numero2 = 20;
+
+        suma.Resultado = suma.Numero1 + suma.Numero2;
+        suma.Resultado = Convert.ToInt32(suma.Numero1) + Convert.ToInt32(suma.Numero2);
+        ViewBag.Resultado = suma.Resultado;
+     
+        return View();
+    }
+}
---
+++ WebApplication1/Models/suma.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class Suma
+{
+    [Required]
+    public int Numero1 { get; set; }
+    [Required]
+    public int Numero2 { get; set; }
+    [Editable(false)]
+    public int Resultado { get; set; }
+
+}
